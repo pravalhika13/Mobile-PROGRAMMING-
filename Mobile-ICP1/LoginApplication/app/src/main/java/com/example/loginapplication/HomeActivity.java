ab7c33ffae45d3fc6f3132337c1d7189f2e4b713
pravalhika13 <42563201+pravalhika13@users.noreply.github.com>
2020-04-16 10:40:11 -0500

    commit
    commit

---
+++ Mobile-ICP1/LoginApplication/app/src/main/java/com/example/loginapplication/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.loginapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.loginapplication.R;
+
+public class HomeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+    public void logout(View view){
+        Intent redirect= new Intent(HomeActivity.this,MainActivity.class);
+        startActivity(redirect);
+    }
+}
